e825a8ff01b3a537082f7c674b245d14f712e875
S Syed Muiz Ahmed <mzcse1102@gmail.com>
2025-01-17 14:52:40 +0530

    Finished Gatsby Tutorial Part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 // Step 1: Import React
 import * as React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
